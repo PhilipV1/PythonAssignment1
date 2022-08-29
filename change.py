eacedac2f8e89e19c80a1f3642477da4117be085
PhilipV1 <philipvelandria@gmail.com>
2022-08-29 23:40:22 +0200

    Change task

--- change.py
+++ change.py
@@ -1,24 +1,32 @@
+from math import floor
+
 import pprint
 
-def createDict():
+def createDict(intArray):
     bills = {
-        "1000kr bills": 0,
-        "500kr bills": 0,
-        "200kr bills": 0,
-        "100kr bills": 0,
-        "50kr bills": 0,
-        "20kr bills": 0,
-        "10kr coins": 0,
-        "5kr coins": 0,
-        "2kr coins": 0,
-        "1kr coins": 0
+        "1000kr bills": intArray[0],
+        "500kr bills": intArray[1],
+        "200kr bills": intArray[2],
+        "100kr bills": intArray[3],
+        "50kr bills": intArray[4],
+        "20kr bills": intArray[5],
+        "10 coins": intArray[6],
+        "5kr coins": intArray[7],
+        "2kr coins": intArray[8],
+        "1kr coins": intArray[9]
     }
     return bills
 
-def calcBills(num):
-    retVal = 0
+def calcBills(change):
+    dividerArray = [1000, 500, 200, 100, 50, 20, 10, 5, 2, 1]
+    billArray = [0 for i in range(10)]#Initialize all the value of all indices to 0
+    for index in range(0, len(dividerArray), 1):
+        billAmount = floor(change // dividerArray[index]) #billAmount stores how many bills of current type
+        if billAmount > 0:#Checking if there is a bill of current type
+            billArray[index] = billAmount
+            change -= dividerArray[index] * billAmount#Subtracting the value of the bills to get the remaining change
 
-    return retVal
+    return billArray
 
 def calChange(payment, price):
     if payment > price:
@@ -27,11 +35,17 @@ def calChange(payment, price):
     return 0
 
 def main():
-    bills = createDict()
-    price = 372.38
-    payment = 1000
+    price = 372
+    payment = 358484
+    change = calChange(payment, price)
 
-    pprint.pprint(bills, sort_dicts=False)
+    print(f"Price: {price}")
+    print(f"Payment: {payment}")
+
+    print(f"\nChange: {change}")
+    billDictionary = createDict(calcBills(change)) 
+    pprint.pprint(billDictionary, sort_dicts=False)
+
 
 if __name__ == "__main__":
     main()
