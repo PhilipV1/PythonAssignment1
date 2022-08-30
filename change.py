034987bf527b6a80e616eeba3feb9d76f70dd97b
PhilipV1 <philipvelandria@gmail.com>
2022-08-30 20:30:50 +0200

    Modified change and added new tasks

--- change.py
+++ change.py
@@ -1,39 +1,37 @@
 from math import floor
 
-import pprint
+def calcBills(change, billArray):
+    billAmount = [0 for i in range(len(billArray))]#Initialize all the value of all indices to 0 with the length of billArray
+    for index in range(0, len(billArray), 1):
+        amount = floor(change // billArray[index]) #amount stores how many bills of current type
+        if amount > 0:#Checking if there is a bill of current type
+            billAmount[index] = amount
+            change -= billArray[index] * amount#Subtracting the value of the bills to get the remaining change
 
-def createDict(intArray):
-    #using the intArray from calcBills to assign value to matching key
-    bills = {
-        "1000kr bills": intArray[0],
-        "500kr bills": intArray[1],
-        "200kr bills": intArray[2],
-        "100kr bills": intArray[3],
-        "50kr bills": intArray[4],
-        "20kr bills": intArray[5],
-        "10 coins": intArray[6],
-        "5kr coins": intArray[7],
-        "2kr coins": intArray[8],
-        "1kr coins": intArray[9]
-    }
-    return bills
+    return billAmount
 
-def calcBills(change):
-    dividerArray = [1000, 500, 200, 100, 50, 20, 10, 5, 2, 1]
-    billArray = [0 for i in range(10)]#Initialize all the value of all indices to 0
-    for index in range(0, len(dividerArray), 1):
-        billAmount = floor(change // dividerArray[index]) #billAmount stores how many bills of current type
-        if billAmount > 0:#Checking if there is a bill of current type
-            billArray[index] = billAmount
-            change -= dividerArray[index] * billAmount#Subtracting the value of the bills to get the remaining change
+def calcCoins(change, coinArray):
+    #Read info in calcBills function
+    coinAmount = [0 for i in range(len(coinArray))]
+    for index in range(0, len(coinArray), 1):
+        amount = floor(change // coinArray[index])
+        if amount > 0:
+            coinAmount[index] = amount
+            change -= coinArray[index] * amount
 
-    return billArray
+    return coinAmount
 
-def calChange(payment, price):
+def calcChange(payment, price):
     if payment > price:
         return round(payment) - round(price)
 
     return 0
+
+def calcLeftOver(change, billArray, billAmount):
+    for i in range(0, len(billArray), 1):
+        change -= billArray[i] * billAmount[i]
+        
+    return change
 
 def userInput(string):
     if string == "Price":
@@ -50,20 +48,28 @@ def userInput(string):
                 return payment
             except ValueError:
                 print("Enter a float!")
-    return 0
 
 def main():
     price = userInput("Price")
     payment = userInput("Payment")
-    change = calChange(payment, price)
+    change = calcChange(payment, price)
+    coins = [10, 5, 2, 1]
+    bills = [1000, 500, 200, 100, 50, 20]
 
     print(f"Price: {price}")
     print(f"Payment: {payment}")
-
     print(f"\nChange: {change}")
-    billDictionary = createDict(calcBills(change)) 
-    pprint.pprint(billDictionary, sort_dicts=False)
 
+    #Calculates the amount of bills then calculates the left over change to furhter calculate the amount of coins
+    numOfBills = calcBills(change, bills)
+    change = calcLeftOver(change, bills, numOfBills)
+    numOfCoins = calcCoins(change, coins)
+
+    for i in range(0, len(bills), 1):
+        print(f"{bills[i]}kr bills: {numOfBills[i]}")
+    
+    for i in range(0, len(coins), 1):
+        print(f"{coins[i]}kr coins: {numOfCoins[i]}")
 
 if __name__ == "__main__":
     main()
